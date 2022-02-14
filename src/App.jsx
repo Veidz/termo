c8e4c253d7113fa19800311e4a16e5421e71aec2
fumagalli <la_12_msn@hotmail.com>
2022-02-13 22:55:44 -0300

    config: install prettier and standardizing style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import React from "react";
+import React from 'react';
+import './index.css';
 
 const App = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
 
 export default App;
