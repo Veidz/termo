1359a6c402173981a024a1a3ba1fab8655108203
fumagalli <la_12_msn@hotmail.com>
2022-02-14 20:51:20 -0300

    feat: create Board and Keyboard archives

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Board } from './components';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Board />
+    </div>
   );
 }
 
