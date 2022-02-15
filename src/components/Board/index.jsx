1ccce66c1c0dffb4776145d68303908b42a7a2cd
Veidz <joaovictorveidz@gmail.com>
2022-02-15 01:41:48 -0300

    create some inputs for board and style file

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import style from '../../styles';
+
+const { input } = style;
+
+const gameBoard = [
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+]
+
+const correctAnswer = "hello";
 
 function Board() {
+
+  const handleChange = (index) => {
+    console.log(index);
+  }
+
   return (
-    <div>
-      Has
+    <div className="grid grid-cols-5">
+      {/* First Row */}
+      <input
+        className={ input }
+        type="text"
+        onChange={ () => handleChange(0) }
+        maxLength="1"
+      />
+      <input
+        className={ input }
+        type="text"
+        onChange={ () => handleChange(1) }
+        maxLength="1"
+      />
+      <input
+        className={ input }
+        type="text"
+        onChange={ () => handleChange(2) }
+        maxLength="1"
+      />
+      <input
+        className={ input }
+        type="text"
+        onChange={ () => handleChange(3) }
+        maxLength="1"
+      />
+      <input
+        className={ input }
+        type="text"
+        onChange={ () => handleChange(4) }
+        maxLength="1"
+      />
     </div>
   );
 }
