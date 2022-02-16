45f0fad74010f88ab1a5e9d0fbb04773256af61d
Veidz <joaovictorveidz@gmail.com>
2022-02-16 00:09:54 -0300

    fix lint for windows

--- src/components/SubmitButton/index.jsx
+++ src/components/SubmitButton/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import TermoContext from '../../context/TermoContext';
 import style from '../../style';
 
@@ -15,7 +15,7 @@ function SubmitButton() {
     >
       Submit
     </button>
-  )
+  );
 }
 
 export default SubmitButton;
