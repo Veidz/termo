e7a4c68a14ab2b32604bf2b9cdebec314bd19d81
Veidz <joaovictorveidz@gmail.com>
2022-02-16 19:55:10 -0300

    change eslint curly-spacing

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -17,7 +17,10 @@ const { input } = style;
 // const correctAnswer = 'hello';
 
 function Board() {
-  const { playerAnswer, setPlayerAnswer, colorIndex } = useContext(TermoContext);
+  const {
+    playerAnswer, setPlayerAnswer, colorIndex,
+    inputsRender, userInputs,
+  } = useContext(TermoContext);
   // console.log(colorIndex);
   const handleChange = (selectedPosition, { target }) => {
     const { value } = target;
@@ -34,15 +37,15 @@ function Board() {
       {/* First Row */}
       {colorIndex && (
         colorIndex.map((el) => (
-          <input className={`${input} ${el[1]}`} value={el[0]} />
+          <input className={ `${input} ${el[1]}` } value={ el[0] } />
         ))
       )}
-      <BoardRow handleChange={handleChange} />
-      <BoardRow handleChange={handleChange} />
-      <BoardRow handleChange={handleChange} />
-      <BoardRow handleChange={handleChange} />
-      <BoardRow handleChange={handleChange} />
-      <BoardRow handleChange={handleChange} />
+      <BoardRow handleChange={ handleChange } />
+      <BoardRow handleChange={ handleChange } />
+      <BoardRow handleChange={ handleChange } />
+      <BoardRow handleChange={ handleChange } />
+      <BoardRow handleChange={ handleChange } />
+      <BoardRow handleChange={ handleChange } />
     </div>
   );
 }
